678a7c59b8d147d8dd29e9caa7121d680a47a655
Benoit Bucher <benoit.bucher@yahoo.fr>
2025-05-08 20:50:20 +0200

    MOD to update icon in the stats status in the game screen

--- src/components/game/GameStats.tsx
+++ src/components/game/GameStats.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useGame } from '../../context/GameContext';
-import { Clock, Star } from 'lucide-react';
+import { Hourglass, Star, Skull, X, Flame } from 'lucide-react';
 
 const GameStats: React.FC = () => {
   const { score, timeElapsed, mistakesMade } = useGame();
@@ -14,17 +14,17 @@ const GameStats: React.FC = () => {
   return (
     <div className="flex justify-between items-center max-w-2xl mx-auto bg-zinc-900/50 backdrop-blur-sm rounded-lg p-2 md:p-4 border border-zinc-800">
       <div className="flex items-center space-x-1 md:space-x-2">
-        <Clock size={16} className="text-sky-300 md:scale-125" />
+        <Hourglass size={16} className="text-ghost md:scale-125" />
         <span className="text-base md:text-xl font-medium">{formatTime(timeElapsed)}</span>
       </div>
       
       <div className="flex items-center space-x-1 md:space-x-2">
-        <Star size={16} className="text-yellow-300 md:scale-125" />
+        <Flame size={18} className="text-green-400 md:scale-125" />
         <span className="text-base md:text-xl font-medium">{score}</span>
       </div>
       
       <div className="flex items-center space-x-1 md:space-x-2">
-        <span className="text-red-400 text-[10px] md:text-xs uppercase font-medium">Mistakes</span>
+        <X size={18} className="text-red-400 md:scale-125" />
         <span className="text-base md:text-xl font-medium">{mistakesMade}</span>
       </div>
     </div>
