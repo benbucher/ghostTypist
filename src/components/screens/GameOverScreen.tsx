a3f97597cd39755861fd0632d902e99bbcaddf99
Benoit Bucher <benoit.bucher@yahoo.fr>
2025-05-09 11:16:43 +0200

    MOD to align responsivity of gameoverscreen on the startscreen one

--- src/components/screens/GameOverScreen.tsx
+++ src/components/screens/GameOverScreen.tsx
@@ -40,15 +40,14 @@ const GameOverScreen: React.FC = () => {
       <div className="relative z-10 space-y-8">
         <div className="mb-8 animate-float">
           <GhostIcon 
-            size={128}
+            size={96}
             strokeWidth={1.8}
-            className="mx-auto text-red-300/80 opacity-90 animate-pulse"
+            className="mx-auto text-red-300/80 opacity-90 animate-pulse md:w-32 md:h-32 w-24 h-24"
           />
         </div>
         
         <motion.h1 
-          // className="text-5xl font-bold mb-4 tracking-tighter text-red-300"
-          className="text-7xl font-medium mb-4 tracking-tight text-red-300"
+          className="text-5xl md:text-7xl font-medium mb-4 tracking-tight text-red-300"
           initial={{ y: -20 }}
           animate={{ y: 0 }}
         >
@@ -56,7 +55,7 @@ const GameOverScreen: React.FC = () => {
         </motion.h1>
         
         <motion.div 
-          className="bg-gray-800 text-zinc-400 text-xl space-y-2 rounded-lg p-6 w-full max-w-xs mx-auto"
+          className="bg-gray-800 text-zinc-400 text-sm md:text-xl space-y-2 rounded-lg p-4 md:p-6 w-full max-w-xs mx-auto"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.3 }}
@@ -86,7 +85,7 @@ const GameOverScreen: React.FC = () => {
         
         <motion.button 
           onClick={handleTryAgain}
-          className="px-8 py-3 bg-ghost-dark/90 hover:bg-ghost-dark text-white rounded-lg text-lg font-medium transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-ghost-dark/20 focus:outline-none focus:ring-2 focus:ring-ghost/50"
+          className="px-6 md:px-8 py-2.5 md:py-3 bg-ghost-dark/90 hover:bg-ghost-dark text-white rounded-lg text-base md:text-lg font-medium transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:shadow-ghost-dark/20 focus:outline-none focus:ring-2 focus:ring-ghost/50"
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
